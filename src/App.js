c840a1a4c3f91b0946e3028264820388b3e6c5da
Martin Racana <ing.racana@gmail.com>
2020-05-01 20:43:02 +0200

    creating Home/user/Fav pages and buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+import { NavBar } from './components/NavBar'
 import { Router } from '@reach/router'
 
 export const App = () => {
@@ -14,8 +19,11 @@ export const App = () => {
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        <NotRegisteredUser path
+        <Favs path='/favs' />
+        <User path='user' />
       </Router>
-
+      <NavBar />
     </>
   )
 }
