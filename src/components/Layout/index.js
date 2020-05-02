022a77533e0d8c0f5e5cd18002425b56d4acaaa7
Martin Racana <ing.racana@gmail.com>
2020-05-02 19:31:52 +0200

    adding title and meta dinamic with react Helmet

---
+++ src/components/Layout/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { Div, Title, Subtitle } from './styles'
+export const Layout = ({ children, title, subtitle }) => (
+  <>
+    <Helmet>
+      {title && <title>{title}| Incarigram 👻 </title>}
+      {subtitle &&
+        <meta name='description' content={subtitle} />}
+    </Helmet>
+    <Div>
+      {title && <Title>{title}</Title>}
+      {subtitle && <Subtitle>{subtitle}</Subtitle>}
+      {children}
+    </Div>
+
+  </>
+)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 import { ListOfCategories } from '../components/ListOfCategories'
-
+import { Helmet } from 'react-helmet'
 export const Home = ({ id }) => {
-  return (<>
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={id} />
-          </>)
+  return (
+    <>
+      <Helmet>
+        <title>
+          Tu nueva app de fotos
+        </title>
+        <meta name='description' content='Descubre nuevas imágenes todos los días' />
+      </Helmet>
+      <ListOfCategories />
+      <ListOfPhotoCards categoryId={id} />
+    </>)
 }
